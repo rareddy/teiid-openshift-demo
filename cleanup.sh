19b8cb7453c478ffaa8d3a251f194f8074a7b9db
Ramesh Reddy <rareddy@jboss.org>
2018-02-06 10:03:57 -0600

    Correcting the FMP version

---
+++ cleanup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+
+oc delete deploymentconfigs teiid-openshift-demo
+oc delete buildconfig teiid-openshift-demo-s2i
+oc delete route teiid-openshift-demo-odata
+oc delete service teiid-openshift-demo-odata
+oc delete service teiid-openshift-demo-jdbc
+oc delete is teiid-openshift-demo
+
